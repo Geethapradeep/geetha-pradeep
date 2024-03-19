c21947d1f5f98e3ee041db7665985c340f7d142a
Geethapradeep <162981899+Geethapradeep@users.noreply.github.com>
2024-03-19 06:22:46 -0400

    Add files via upload

---
+++ day 2.py
@@ -0,0 +1,92 @@
+
+
+Day 2
+
+#a,b,c = 9,8,7
+#print(a,b,c)
+
+#a, b=c=7, 8
+#prin(a)
+#print(b)
+#print(c)
+ 
+#a=b, c = 4, 2
+#print(a, b, c)
+
+#---->  swapping variable
+a = 7
+b = 5
+
+eg:1
+
+temp=a
+a=b
+b=temp
+print(a,b)
+#a=5, b=7
+print(a,b)
+
+eg:2
+a=5
+b=7
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+
+a=a*b
+b=a/b
+a=a/b
+print(int(a), int(b))
+
+id() --> used to find the memory address of the variables
+a = 7
+b = 8
+print(id(a))
+print(id(b))
+
+# !--> literals
+literal is the constant value assigned to a variable
+a variable is considers to be constant when it is defines in caps
+
+a= 78 # a is variable
+
+A=78 # Ais constant
+
+hash(--> it creates a hash value for constrant datatypes and provides error for non constant data types
+n1 = 89+7j
+print(hash(n1))
+
+
+f1 = [7, 8, 9]
+print(hash(f1)) # error --> list is non-constant or multiable datatypes
+     
+a = 9
+b = 90
+
+
+# ! ---> operators
+# ? Operators are symbols which is used to perform various oprations betwee 2 0r more operands
+ arithmatic
+ assignment
+ logical
+ relational or comprison
+ bitwise
+ identity
+ membership
+
+ # todo ---> arithmatic --> +, -, *, /, %, //, **
+ eg 1
+     a = 8
+     b = 6
+     c = 9
+     print(a+b+c)
+      input () ---> used to get the runtime input
+     n1 = input ("enter the value")
+     print(n1)
+     
+
+
+
+
+     
